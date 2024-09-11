3b948421b78b6b6c6a7dbbf446d85671dd72afea
Victor Lucassen <vict7255@gmail.com>
2024-09-11 10:26:33 +0200

    Update classifierAndPropertyCount.py

--- classifierAndPropertyCount.py
+++ classifierAndPropertyCount.py
@@ -99,8 +99,8 @@ def property_counter(array):
     global inProperty
 
     for i in range(5):
-        for j in range(5):
-            if inProperty == False and j > 0 and (array[i][j] == array[i][j-1]):
+        for j in range(4):
+            if inProperty == False and j < 4 and (array[i][j] == array[i][j+1]):
                 property_array = np.insert(property_array, property_count, 1)
 
                 property_count += 1
@@ -111,7 +111,7 @@ def property_counter(array):
 
                 inProperty = True
 
-            if inProperty == True and j > 0 and (array[i][j] == array[i][j-1]) and j<5:
+            if inProperty == True and j < 4 and (array[i][j] == array[i][j+1]):
                 property_array[-1] += 1
 
                 if i<4 and array[i+1][j] == array[i][j]:
@@ -122,5 +122,7 @@ def property_counter(array):
     #wtf i will shutdown the live share right now
     print(property_array)
 
-property_counter(classification_array)
+#property_counter(classification_array)
+
+#if right = left, if top = bottom
                 
