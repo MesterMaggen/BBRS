5ae905fe1df852b46a447396d8371a95225214c1
MesterMaggen <101719834+MesterMaggen@users.noreply.github.com>
2024-09-12 08:58:58 +0200

    Update FrequencyCounter.py

--- FrequencyCounter.py
+++ FrequencyCounter.py
@@ -2,33 +2,27 @@ import cv2 as cv
 import numpy as np
 from collections import Counter
 
-image = cv.imread("King Domino dataset/Cropped and perspective corrected boards/55.jpg", cv.IMREAD_COLOR)
+image = cv.imread("King Domino dataset/Cropped and perspective corrected boards/9.jpg", cv.IMREAD_COLOR)
 new_image = np.zeros((500, 500, 3),dtype=np.uint8)
 
 for tileRow in range(5):
     for tileColumn in range(5):
         tempimg = image[tileRow*100:(tileRow+1)*100, tileColumn*100:(tileColumn+1)*100]
-        
+
         for y, row in enumerate(tempimg):
             for x, pixel in enumerate(row):
                 tempimg[y,x,0] = (round(np.float64(pixel[0]) / 9) * 9)
                 tempimg[y,x,1] = (round(np.float64(pixel[1]) / 9) * 9)
                 tempimg[y,x,2] = (round(np.float64(pixel[2]) / 9) * 9)
-
+        
         tempimg = [tuple(tempimg[i][j]) for i in range(100) for j in range(100)]
         counted = Counter(tempimg)
-        most_common_object, most_common_count = counted.most_common(2)[1]
-        print(most_common_object)
+        most_common_object, most_common_count = counted.most_common(2)[0]
+        print("Tile[",tileRow,",",tileColumn,"]:",counted.most_common(2))
         new_image[tileRow*100:(tileRow+1)*100, tileColumn*100:(tileColumn+1)*100] = most_common_object
-        #print("(", tileRow, ",", tileColumn, "):", most_common_object)
 
-#cv.imshow("Forest square", new_image[0:100,300:400])
-#print("Forest square", new_image[50,350])
-#cv.imshow("Mine square", new_image[200:300,0:100])
-#print("Mine square", new_image[250,50])
-
-# Save or display the new image
 cv.imshow("Original Image", image)
-cv.imshow("New Image", new_image)
+cv.imshow("New RGB", new_image)
+#cv.imshow("New HSV", cv.cvtColor(new_image, cv.COLOR_BGR2HSV))
 cv.waitKey(0)
 cv.destroyAllWindows()
