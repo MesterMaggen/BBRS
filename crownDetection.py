f9084fe76303422d6081ce0ae372e1b271394cad
MesterMaggen <101719834+MesterMaggen@users.noreply.github.com>
2024-09-17 16:19:14 +0200

    Update crownDetection.py

--- crownDetection.py
+++ crownDetection.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-image = cv.imread("King Domino dataset/Cropped and perspective corrected boards/1.jpg", cv.IMREAD_COLOR)
+image = cv.imread("King Domino dataset/Cropped and perspective corrected boards/2.jpg", cv.IMREAD_COLOR)
 #new_image = np.zeros((500, 500, 3),dtype=np.uint8)
 
 sharpening_kernel = np.array([[-1, -1, -1],
-                              [-1,  10, -1],
+                              [-1,  9, -1],
                               [-1, -1, -1]])
 
 sharpened_image = cv.filter2D(src=image, ddepth=-1, kernel=sharpening_kernel)
